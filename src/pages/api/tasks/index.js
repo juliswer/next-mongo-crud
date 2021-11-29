b74e4675310c510add486ef2244fc088b9722542
Julian Swerdlin <julianswer36@gmail.com>
2021-11-29 18:36:40 +0100

    uploading models files

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -1,7 +1,10 @@
-import {dbConnect} from '../../../utils/mongoose';
+import {dbConnect} from 'utils/mongoose';
+import Task from 'models/Task';
 
 dbConnect()
 
-export default function handler(req, res) {
-    res.status(200).json("tasks")
+export default async function handler(req, res) {
+    const tasks = await Task.find();
+    console.log(tasks);
+    res.status(200).json(tasks)
 }  
