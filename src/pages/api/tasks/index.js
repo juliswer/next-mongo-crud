df01e6aa6ed7f8f203bf04ddf122e3dbc9cbf55f
Julian Swerdlin <julianswer36@gmail.com>
2021-11-30 23:11:36 +0100

    uploading models files

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -6,5 +6,6 @@ dbConnect()
 export default async function handler(req, res) {
     const tasks = await Task.find();
     console.log(tasks);
+    console.log("llegado")
     res.status(200).json(tasks)
 }  
