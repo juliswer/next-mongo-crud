299eb09948b4ff1084a72bf7d1ad65f15626faa2
Julian Swerdlin <julianswer36@gmail.com>
2021-11-30 23:34:17 +0100

    uploading models"

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -4,8 +4,17 @@ import Task from 'models/Task';
 dbConnect()
 
 export default async function handler(req, res) {
-    const tasks = await Task.find();
-    console.log(tasks);
-    console.log("llegado")
-    res.status(200).json(tasks)
+
+    const {method, body} = req
+
+    switch (method) {
+        case "GET": 
+            const tasks = await Task.find();
+            return res.status(200).json(tasks)
+        case "POST":
+            console.log(body);
+            return res.json("creating a task")
+        default:
+            return res.status(400).json({msg: "This method is not supported"});
+    }
 }  
