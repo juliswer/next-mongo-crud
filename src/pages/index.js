deec408900b42c429fe2e5482e1cde194298b195
Julian Swerdlin <julianswer36@gmail.com>
2021-12-01 16:03:18 +0100

    uploading front files

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,3 +5,14 @@ export default function HomePage() {
     </div>
   )
 }
+
+export async function getServerSideProps(){
+  
+  const res = await fetch('http://localhost:3000/api/tasks')
+  const tasks = await res.json()
+  console.log(tasks);
+  
+  return {
+    props: {}
+  }
+}
